6efeddcc7e37decc37f2bf5385f97b0b323f3951
FlorentUguet <florent.uguet@gmail.com>
2015-11-13 20:55:41 +0100

    Guide ressources

--- src/ressources.h
+++ src/ressources.h
@@ -15,8 +15,26 @@ enum Actions
     ACTION_END_LIST
 };
 
+enum GuideType
+{
+    G_GENERAL =0,
+    G_PERSONALITY,
+    G_SENTIENCE,
+    G_VISUALIZATION,
+    G_CONCENTRATION,
+    G_VOCALIZATION,
+    G_FORCING,
+    G_MOTIVATION,
+    G_IMPOSITION,
+    G_POSSESSION,
+    G_OTHER,
+    G_END_LIST
+};
+
 const QString titles[OBJECT_NUMBER] = {"Session","Tulpa","Action type","Date","Time spent (Minutes)","Detailed description"};
 const QString actions_string[ACTION_END_LIST] = {"Active Forcing (Talking)","Active Forcing (Visualisation)","Active Forcing (Both)","Passive Forcing","Meditation"};
+
+const QString guide_types[G_END_LIST] = {"GENERAL","PERSONALITY","SENTIENCE","VISUALIZATION","CONCENTRATION","VOCALIZATION","FORCING","MOTIVATION","IMPOSITION","POSSESSION","OTHER"};
 
 #endif // RESSOURCES
 
