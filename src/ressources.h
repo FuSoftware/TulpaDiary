5d7a12211523fac736808185fae0889818e9e07b
FlorentUguet <florent.uguet@gmail.com>
2015-11-22 17:28:21 +0100

    Added TextEditWriting

--- src/ressources.h
+++ src/ressources.h
@@ -3,7 +3,7 @@
 
 #include <QString>
 
-#define OBJECT_NUMBER 6
+#define OBJECT_NUMBER 7
 
 enum Actions
 {
@@ -31,7 +31,7 @@ enum GuideType
     G_END_LIST
 };
 
-const QString titles[OBJECT_NUMBER] = {"Session","Tulpa","Action type","Date","Time spent (Minutes)","Detailed description"};
+const QString titles[OBJECT_NUMBER] = {"Session","Tulpa","Action type","Date","Time spent (Minutes)","Detailed description", "Session's writing"};
 const QString actions_string[ACTION_END_LIST] = {"Active Forcing (Talking)","Active Forcing (Visualisation)","Active Forcing (Both)","Passive Forcing","Meditation"};
 
 const QString guide_types[G_END_LIST] = {"GENERAL","PERSONALITY","SENTIENCE","VISUALIZATION","CONCENTRATION","VOCALIZATION","FORCING","MOTIVATION","IMPOSITION","POSSESSION","OTHER"};
@@ -45,7 +45,7 @@ enum Tutorials{
 };
 
 const QString tutorials[TUTO_END_LIST]= {"To edit a date's sessions, open the date by double-clicking it",
-                                         "Here are listed all the sessions affected to the current date.\nPlease note that for now, you'll have to re-open the date to refresh the sessions list.\nThey are not automatically refreshed",
+                                         "Here are listed all the sessions affected to the current date.\nYou can add, edit, or delete sessions.",
                                          ""};
 
 #endif // RESSOURCES
